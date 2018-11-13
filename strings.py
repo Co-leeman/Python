12a6c07ae8d4d3945d3a3b6397ee59d857537386
co-leeman <temple.lee.coleman@outlook.com>
2018-11-13 00:13:47 -0700

    first commit

---
+++ strings.py
@@ -0,0 +1,21 @@
+
+file = open("file.txt","w")
+
+file.write("HEllo world")
+
+file.close()
+#python nombre_dearchivo para correrlo
+#cd Folder para ir a ese folder
+#cd ../ para ir a folder anterior
+ez
+
+also, recomendations for ordering stuff
+
+dunno folders i dont organize
+BUT
+
+vs code tiene la ventaja que tiene git integrado si aprendes a usarlo puede ser util
+pero es mucho para explicar capaz sea mejor buscar un tutorial
+
+basically de aqui puedes subir y bajar versiones de tu projecto de github para tenerlo en todos lados
+
