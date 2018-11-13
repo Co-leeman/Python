4ce7ecd19dce297539f76c7fd01e99d374af5a49
co-leeman <temple.lee.coleman@outlook.com>
2018-11-13 00:15:32 -0700

    added shit

--- strings.py
+++ strings.py
@@ -7,3 +7,6 @@ file.close()
 #python nombre_dearchivo para correrlo
 #cd Folder para ir a ese folder
 #cd ../ para ir a folder anterior
+
+#NotADirectoryError
+#k cuando quieras updatear este proyecto te saldra aqui y le pones un mensaje y en la flechita
